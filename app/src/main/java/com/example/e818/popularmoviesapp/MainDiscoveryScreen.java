69f576d12887e61fbc6ef749ee6ad83ed0343c6b
Daniel Gomes <wormdaniel_69@hotmail.com>
2018-06-01 11:59:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/e818/popularmoviesapp/MainDiscoveryScreen.java
@@ -0,0 +1,13 @@
+package com.example.e818.popularmoviesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDiscoveryScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_discovery_screen);
+    }
+}
